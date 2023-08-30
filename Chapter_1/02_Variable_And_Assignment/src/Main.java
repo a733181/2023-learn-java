5555070653f62affaa8f8e1182d822b829bc1743
a733181 <a733181@gmail.com>
2023-08-30 21:57:32 +0800

    Variable_And_Assignment

---
+++ Chapter_1/02_Variable_And_Assignment/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5, y = 6;
+        x++;
+        System.out.println(x + y);
+
+        final double PI = 3.14;
+    }
+}
