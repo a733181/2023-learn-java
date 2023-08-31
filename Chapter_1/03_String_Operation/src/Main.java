6883800f6c3c26ec6b2f4ae8b363c8916d8dad78
a733181 <a733181@gmail.com>
2023-08-31 22:52:45 +0800

    String_Operation

---
+++ Chapter_1/03_String_Operation/src/Main.java
@@ -0,0 +1,15 @@
+// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
+// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tom";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(0));
+        System.out.println(name.indexOf("om"));
+
+        String age ="20";
+        System.out.println(Integer.parseInt(age));
+    }
+}
