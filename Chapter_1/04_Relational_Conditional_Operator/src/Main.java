5212b6cbc983bad51c3fc830850c7484c3740b06
a733181 <a733181@gmail.com>
2023-09-01 21:17:32 +0800

    Relational_Conditional_Operator

---
+++ Chapter_1/04_Relational_Conditional_Operator/src/Main.java
@@ -0,0 +1,17 @@
+// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
+// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(!false);
+        System.out.println(3==3);
+        System.out.println("---");
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || false);
+        System.out.println("---");
+        System.out.println("Tom".equals("tom"));
+        System.out.println("Tom".equals("Tom"));
+    }
+}
