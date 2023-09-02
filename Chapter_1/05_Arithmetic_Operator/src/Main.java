3d4e51df686c789ddaf404d847bb7e67e88f8a90
a733181 <a733181@gmail.com>
2023-09-02 13:58:57 +0800

    Arithmetic_Operator

---
+++ Chapter_1/05_Arithmetic_Operator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("15" + "15");
+        System.out.println(15 + 15);
+        System.out.println(15 + "15");
+        System.out.println(15 + 15 + "15" + 15);
+        System.out.println("---");
+        System.out.println(9 / 10);
+        System.out.println(9.0 / 10);
+    }
+}
