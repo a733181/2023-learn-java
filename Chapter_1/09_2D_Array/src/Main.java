4d64b09dc91e544e043bcb3d8a7e3de9edc46349
a733181 <a733181@gmail.com>
2023-09-07 07:21:03 +0800

    2D_Array

---
+++ Chapter_1/09_2D_Array/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> intArray = new ArrayList<>();
+        intArray.add(10);
+
+        int[][] intArrays = new int[2][2];
+        intArrays[0][1] = 10;
+    }
+}
