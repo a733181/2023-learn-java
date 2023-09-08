23115460479d5e9c183cc36efdd8d53ae3d52c9a
a733181 <a733181@gmail.com>
2023-09-08 19:47:38 +0800

    For_Loop

---
+++ Chapter_1/10_For_Loop/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        String[] friends = {"friend1", "friend2", "friend3" };
+        for (String friend : friends) {
+            System.out.println(friend);
+        }
+
+    }
+}
