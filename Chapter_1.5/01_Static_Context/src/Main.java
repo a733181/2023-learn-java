5dd7542050efcae64111b3aed93f46970b85ae9a
a733181 <a733181@gmail.com>
2023-09-18 22:27:46 +0800

    Static_Context

---
+++ Chapter_1.5/01_Static_Context/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(add(2, 5));
+    }
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
