09c31836020c788ca9d122900c4adf88b6cd4d2a
a733181 <a733181@gmail.com>
2023-09-22 21:25:50 +0800

    Math_Class

---
+++ Chapter_1.5/05_Math_Class/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 28;
+        int b = 4;
+        double c = 3.14;
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.floor(c));
+        System.out.println(Math.ceil(c));
+        System.out.println(Math.sqrt(b));
+
+        System.out.println(Math.pow(a, b));
+        System.out.println(Math.log10(b));
+    }
+}
