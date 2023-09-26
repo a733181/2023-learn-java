82c7b5cb12e48b1920af8b12e12da4eeff3ab349
a733181 <a733181@gmail.com>
2023-09-26 20:55:06 +0800

    Public_and_Private_Attributes

---
+++ Chapter_2/03_Public_and_Private_Attributes/src/Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public double radius1;
+    private double radius2;
+
+    public Circle(double newRadius) {
+        radius2 = newRadius;
+    }
+}
---
+++ Chapter_2/03_Public_and_Private_Attributes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(5);
+        c1.radius1 = 10;
+        System.out.println(c1.radius1);
+//        c1.radius2 =20;
+    }
+}
