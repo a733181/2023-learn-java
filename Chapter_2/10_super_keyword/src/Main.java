c31b4d98f9a45f4fd55feb936f2296e8a4f6f9bb
a733181 <a733181@gmail.com>
2023-10-03 22:29:22 +0800

    super_keyword

---
+++ Chapter_2/10_super_keyword/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        People p1 = new People();
+        p1.soutBasic();
+        Teacher t1 = new Teacher();
+        t1.soutBasic();
+        t1.printHello();
+    }
+}
