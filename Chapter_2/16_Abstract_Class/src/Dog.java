b3b2d2dc8b1065325f67b9752471a917ea59cbae
a733181 <a733181@gmail.com>
2023-10-09 20:10:29 +0800

    Abstract_Class

---
+++ Chapter_2/16_Abstract_Class/src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal {
+
+
+    public Dog() {
+        super("Dog");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Dog Sound");
+    }
+}
