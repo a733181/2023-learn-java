1928d5011d605fa8c484a8cab2d46737f61db006
a733181 <a733181@gmail.com>
2023-11-10 21:18:07 +0800

    Short-circuit _valuation

---
+++ Chapter_3/08_Short-circuit _valuation/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 0;
+        boolean bn1 = false;
+        if (bn1 && (n1 / n2) != 3) {
+            System.out.println(" n1 / n2 ");
+        }
+    }
+}
