ff932f524141056a92e968ea71b4f78ae58b9dd7
a733181 <a733181@gmail.com>
2023-11-11 13:25:29 +0800

    Method_Chaining

---
+++ Chapter_3/09_Method_Chaining/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person tom = new Person();
+        tom.setAge(25).setName("Tom");
+    }
+}
---
+++ Chapter_3/09_Method_Chaining/src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String name;
+    private int age;
+
+
+    public Person() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
